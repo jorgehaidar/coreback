22bbebdc6129f34ddae461a57bb21c42b1fe5518
Jorge Haidar Martinez <77126566+jorgehaidar@users.noreply.github.com>
2024-12-06 10:24:42 -0500

    feat[query_logs]: agregadas las trazas de las consultas a base de datos

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Database\Events\QueryExecuted;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +27,21 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+
+        RateLimiter::for('api', function (Request $request) {
+            return [
+                Limit::perSecond(20)->by(optional($request->user())->id ?: $request->ip()),
+                Limit::perMinute(60)->by(optional($request->user())->id ?: $request->ip())
+            ];
+        });
+
+        $log = env('LOG_QUERY', false);
+        if ($log)
+            DB::listen(function (QueryExecuted $query) {
+                File::append(
+                    storage_path('/logs/query.log'),
+                    $query->sql . ' [' . implode(', ', $query->bindings) . ']' . '[' . $query->time . ']' . PHP_EOL
+                );
+            });
     }
 }
