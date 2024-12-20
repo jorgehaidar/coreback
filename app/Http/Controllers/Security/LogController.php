b9ade73d21d45cb142a4d42a4bbea59b34256d52
Jorge Haidar Martinez <77126566+jorgehaidar@users.noreply.github.com>
2024-12-20 17:47:45 -0500

    feat[logs]: creados los servicios de trazas

---
+++ app/Http/Controllers/Security/LogController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Security;
+
+use App\Http\Controllers\CoreController;
+use App\Models\Security\Log;
+use App\Services\Security\LogService;
+use Carbon\Carbon;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogController extends CoreController
+{
+    public function __construct()
+    {
+        $this->service = new LogService();
+    }
+}
---
+++ app/Services/Security/LogService.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Services\Security;
+
+use App\Services\CoreService;
+use App\Models\Security\Log;
+use Carbon\Carbon;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogService extends CoreService
+{
+    public function __construct()
+    {
+        $this->modelClass = resolve(Log::class);
+    }
+
+    static public function createLoginLogout($action)
+    {
+        $model = Log::create([
+            'users_id' => auth()->check() ? auth()->user()->id : 1,
+            'date_time' => Carbon::now(),
+            'action_name' => $action,
+            'ip' => \request()->ip(),
+            'record' => 'login',
+            'table_name' => 'User',
+        ]);
+
+        if (!$model){
+            return response()->json([
+                'message' => 'Error while creating Log'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return true;
+    }
+
+    static public function createAny($action, $table_name, $record)
+    {
+        $model = Log::create([
+            'users_id' => auth()->check() ? auth()->user()->id : 1,
+            'date_time' => Carbon::now(),
+            'action_name' => $action,
+            'ip' => \request()->ip(),
+            'record' => $record,
+            'table_name' => $table_name,
+        ]);
+
+        if (!$model){
+            return response()->json([
+                'message' => 'Error while creating Log'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return true;
+    }
+}
