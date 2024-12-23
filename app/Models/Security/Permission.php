f00c9f44e79dbad6bb3a353faebcf5951195101b
Jorge Haidar Martinez <77126566+jorgehaidar@users.noreply.github.com>
2024-12-22 20:12:04 -0500

    feat[permissions]: creados los servicios de permisos

---
+++ app/Models/Security/Permission.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models\Security;
+
+use App\Models\CoreModel;
+
+class Permission extends CoreModel
+{
+
+    protected $fillable = [
+        'roles_id',
+        'routes_id'
+    ];
+
+    public function rules($scenario = 'create'): array
+    {
+        $rules = [
+            'create' => [
+                'roles_id' => 'required|exists:roles,id',
+                'routes_id.*' => 'required|integer|exists:routes,id',
+            ],
+            'update' => [
+                'roles_id' => 'required|exists:roles,id',
+                'routes_id.*' => 'required|integer|exists:routes,id',
+            ],
+        ];
+        return $rules[$scenario] ?? [];
+    }
+}
